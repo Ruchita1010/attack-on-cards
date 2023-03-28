6e8be39be68c3c46208f548853fa686cae60ca74
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-03-28 16:35:46 +0530

    feat: add score board Update scores depending on the clicked card. If same card is clicked reset game.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from '../styles/Card.module.css';
 
-const Card = ({ cardContent }) => {
+const Card = ({ cardContent, handleCardClick }) => {
   const { imgPath, infoText } = cardContent;
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleCardClick}>
       <img src={imgPath} alt={infoText} />
       <p className={styles.info_text}>{infoText}</p>
     </div>
--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 import getCards from '../utils/cards';
 import styles from '../styles/GameScreen.module.css';
 
 const GameScreen = () => {
   const cards = getCards();
+  // to keep track of the cards clicked by the user
+  const [clickedCards, setClickedCards] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const resetGame = () => {
+    if (currentScore > bestScore) {
+      setBestScore(currentScore);
+    }
+    setCurrentScore(0);
+    setClickedCards([]);
+  };
+
+  const handleCardClick = (e) => {
+    const cardName = e.target.alt;
+    if (clickedCards.includes(cardName)) {
+      resetGame();
+      return;
+    }
+    setCurrentScore((prevState) => prevState + 1);
+    setClickedCards((prevState) => [...prevState, cardName]);
+  };
+
   return (
     <div className={styles.game_screen}>
+      <div className={styles.score_board}>
+        <p>CURRENT SCORE: {currentScore}</p>
+        <p>BEST SCORE: {bestScore}</p>
+      </div>
       <div className={styles.cards_grid}>
         {cards.map((card) => (
-          <Card key={card.id} cardContent={card} />
+          <Card
+            key={card.id}
+            cardContent={card}
+            handleCardClick={handleCardClick}
+          />
         ))}
       </div>
     </div>
